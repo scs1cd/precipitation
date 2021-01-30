479d46b5293e999f0ad74154f4744e92e27241dd
scs1cd <c.davies@leeds.ac.uk>
2021-01-30 14:04:10 +0000

    B18_final and eq_comp
    I have split up the original notebook Badri18... into 2 notebooks,also retaining the original. B18_final is a condensed notebook that is just trying to replicate B18 Fig 3. It will contribute 1 figure to the paper- XMgO vs T. Need to add different core compositions.
    
    en_comp focuses on just the ideal partitioning, ie. all gammas set to zero. This has been modified so that the mantle composition can be varied. Needs some work to pretty this up.

--- b18_routines.py
+++ b18_routines.py
@@ -5,7 +5,7 @@ AO, AMg, AFe, ASi = 16, 24, 56, 28
 
 def mole2massconc_core(cbarO, cbarSi, cbarMgO):
 
-    AMgO = AMg + AO
+    AMgO = AMg #+ AO
     Abar = cbarO*AO + cbarSi*ASi + cbarMgO*AMgO + (1-cbarO-cbarSi-cbarMgO)*AFe
     cO   = cbarO  * AO  /Abar
     cSi  = cbarSi * ASi /Abar
@@ -14,7 +14,7 @@ def mole2massconc_core(cbarO, cbarSi, cbarMgO):
 
 def mass2moleconc_core(cO, cSi, cMgO):
 
-    AMgO = AMg + AO
+    AMgO = AMg # AO
     Abar = 1 / (cO/AO + cSi/ASi + cMgO/AMgO + (1-cO-cSi-cMgO)/AFe)
     cbarO  = cO  / (AO  /Abar)
     cbarSi = cSi / (ASi /Abar)
@@ -87,9 +87,9 @@ def ln_gamma_i(X, eps, lngammaFe, lngammai0, v):
     i = 0
     for xi in X:
         j = 0
-        if xi == 0 : 
-            i = i + 1
-            continue     
+        #if xi == 0 : 
+        #    i = i + 1
+        #    continue     
         for xj in X:
             if xj == 0 or i==j:  
                 j = j + 1
@@ -113,3 +113,81 @@ def ln_gamma_i(X, eps, lngammaFe, lngammai0, v):
         print('Sum3 = ', tmp3)
             
     return lngammaFe + lngammai0 - T1 - T2 + T3
+
+def min_conc_closure():
+
+    params = {}
+
+    def min_conc(XMg, XO, XSi, XMgO_m, eps, lngamma0, logKd_MgO_B18, T):
+            
+        XO_tot = XO #+ XMg
+        X_C    = np.array( [0.0 , XO_tot, XSi  , 0.0   , XMg] )
+
+        verb = 0
+    
+        lngammaFe  = ln_gamma_Fe(X_C,eps,verb)                       # eqn S3
+        lngammai   = ln_gamma_i( X_C,eps, lngammaFe, lngamma0, verb) # eqn S4
+        
+        loggammaO  = lngammai[1]/2.303
+        loggammaMg = lngammai[4]/2.303
+
+        XMg_ni  = Mg_dissolution(XMgO_m, 10**logKd_MgO_B18,loggammaO,loggammaMg)
+        
+        if verb == 1:
+            # JBs eqn - gives same ans
+            print( np.exp ( logKd_MgO_B18*2.303 + np.log(XMgO_m) - lngammai[1] - lngammai[4] ))
+            print('T = {:6.1f} gammaC  = {:6.4f} gamma_O  = {:6.4f} gammaSi  = {:6.4f} gammaS  = {:6.4f} gammaMg  = {:6.4f} gammaFe  = {:5.3f}'
+           .format(T,np.exp(lngammai[0]),np.exp(lngammai[1]),np.exp(lngammai[2]), p.exp(lngammai[3]),np.exp(lngammai[4]), np.exp(lngammaFe)))
+        
+        params['lngamma']   = lngammai
+        params['lngammaFe'] = lngammaFe
+        params['X']         = np.array( [0.0   , XO_tot   , XSi   , 0.0   , XMg_ni] )
+                
+        return XMg_ni - XMg
+    
+    return params, min_conc
+
+def O_dissociation(XFe, XO, KO):
+    """
+        Returns the molar concentration of FeO in the silicate based on an dissociation reaction
+    """
+    return (XO*XFe) / (KO)
+
+def Si_dissociation(XSi, XO, KSi):
+    """
+        Returns the molar concentration of Si)2 in the silicate based on an dissociation reaction
+    """
+    return (XSi*XO**2) / KSi
+
+def Si_exchange(XSi, XFe, XFeO, KSi):
+    """
+        Returns the molar concentration of Si in the metal based on an exchange reaction
+    """
+    return (XSi/KSi) * (XFeO/XFe)**2
+
+def Mg_exchange(XMgO, XFe, XFeO, KMg, loggammaO, loggammaMg):
+    """B18 eqn 10
+        Returns the molar concentration of Mg in the metal based on an exchange reaction
+    """
+    return 10**( np.log10(KMg) + np.log10(XFe*XMgO) - np.log10(XFeO) - loggammaO - loggammaMg)
+
+
+def Mg_dissolution(XMgO, KMg, loggammaO, loggammaMg):
+    """B18 eqn 9
+        Returns the molar concentration of MgO in the metal based on a dissolution reaction
+    """
+    return 10**( np.log10(KMg) + np.log10(XMgO) - loggammaO - loggammaMg)
+
+
+def Mg_dissolution_B16(XMgO,KMg):
+    """B16 eqn 4
+        Returns the molar concentratino of Mg in the metal based on a dissolution reaction
+    """
+    return 10**( (np.log10(KMg) + np.log10(XMgO))/2.0 )
+
+
+def Mg_dissociation(XMgO, XO, KMg, loggammaO, loggammaMg):
+    """B18 eqn 8
+        Returns the molar concentration of Mg in the metal based on a dissociation reaction
+    """
+    return 10**( np.log(XMgO) + np.log(KMg) - np.log(XO) - loggammaO - loggammaMg)
